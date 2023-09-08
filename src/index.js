d25cef6c9e8031595bbc217bd4a07b8ea8a14f11
Akshat Neerati <78110893+aneerati@users.noreply.github.com>
2023-09-07 22:23:30 -0400

    fixed message issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const {Client, Intents} = require('discord.js');
 const DiscToken  = 'TOKEN';
+const prefix = "$";
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+]});
 
-client.once('ready', () => {
-    console.log("READY!");
-})
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+    if (msg.content.startsWith(prefix)) {
+        const args = msg.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (command === "ping") {
+            msg.channel.send("pong")
+        }
+    }
+    
+});
 
 client.login(DiscToken);
