9012a5be6dd65ee573779ce2e10f7a9c85c3f498
aneerati <aneerati2004@gmail.com>
2023-09-07 23:05:51 -0400

    minor changes to news fetching

--- src/news.js
+++ src/news.js
@@ -1,17 +1,17 @@
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('NEWSTOKEN');
+const newsapi = new NewsAPI('f4efbe81734e4fa0a278995aeac3b265');
 
 newsapi.v2.topHeadlines({
-    category: 'business',
-    country: 'us'
+    category: 'general',
+    country: 'us',
 }).then(response => {
     response.articles.forEach((article, index) => {
         if (article.title.toLowerCase() !== '[removed]') {
-            console.log("-------------");
-            console.log(`Article ${index + 1}: ${article.title}`);
+            console.log("-------------------------------");
+            console.log(`Title: ${article.title}`);
         }    
     });
-    // console.log(response);
+    //console.log(response);
 });
 
 // Fetches the articles
